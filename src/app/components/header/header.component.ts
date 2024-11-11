c9d86c2328708c2afe8dfb8fc4969ccd33373301
amhernandez13 <amhernandez13@outlook.com>
2024-11-11 02:16:53 -0500

    website finished and also made responsive, and translated to spanish

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ScrollService } from '../../services/scroll.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,22 @@ import { ScrollService } from '../../services/scroll.service';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  constructor(private scrollService: ScrollService) {}
+  constructor(
+    private scrollService: ScrollService,
+    private location: Location
+  ) {}
 
   scrollToSection(event: Event, section: string) {
     event.preventDefault();
     this.scrollService.scrollTo(section); // Llama al servicio de desplazamiento
+  }
+
+  switchLanguage() {
+    const currentUrl = this.location.path();
+    const isEnglish = !currentUrl.includes('/es/');
+    const newUrl = isEnglish
+      ? `/es${currentUrl}`
+      : currentUrl.replace('/es', '');
+    window.location.href = newUrl;
   }
 }
